630aba5a73a540f36b3aff593009ea45b8038404
Andrii Tonkonozhenko <andriy201414@ukr.net>
2022-08-07 11:12:00 +0300

    add test structure

---
+++ test/blockStateStructure.test.ts
@@ -0,0 +1,13 @@
+import { BlockState } from '../src/blockStateStructure'
+import { suite, test } from '@testdeck/mocha'
+import * as _chai from 'chai'
+import { expect } from 'chai'
+
+_chai.should()
+_chai.expect
+
+@suite class BlockStateTest {
+    @test 'turnFrontBlockLeft' () {
+        console.log('Віталік, допиши логіку обертання для подальшого тестування!')
+    }
+}
