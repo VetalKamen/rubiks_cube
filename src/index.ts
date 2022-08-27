8b0b529d5a5052dab982d35f1e0beca928cc93bb
Andrii Tonkonozhenko <andriy201414@ukr.net>
2022-08-27 20:23:15 +0300

    Add Cube turn logic and test

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,62 @@
 import RubiksCube from './RubiksCube'
-import {RubiksCubeStateStructure} from './RubiksCubeStateStructure'
+import { RubiksCubeStateStructure } from './RubiksCubeStateStructure'
 import Color from './color'
 import { RubiksCubeAssembler } from './RubicCubeAssembler'
 import TurnService from './TurnService'
 
 const frontSide: Color[] = [
-  Color.Yellow, Color.White, Color.White,
-  Color.White, Color.White, Color.White,
-  Color.White, Color.White, Color.White,
-]
-
-const lefttSide: Color[] = [
-  Color.Blue, Color.Blue, Color.Blue,
-  Color.Blue, Color.Blue, Color.Blue,
-  Color.Blue, Color.Blue, Color.Blue,
+    Color.White, Color.White, Color.White,
+    Color.White, Color.White, Color.White,
+    Color.White, Color.White, Color.White,
 ]
 
 const rightSide: Color[] = [
-  Color.Green, Color.Green, Color.Green,
-  Color.Green, Color.Green, Color.Green,
-  Color.Green, Color.Green, Color.Green,
+    Color.Blue, Color.Blue, Color.Blue,
+    Color.Blue, Color.Blue, Color.Blue,
+    Color.Blue, Color.Blue, Color.Blue,
+]
+
+const leftSide: Color[] = [
+    Color.Green, Color.Green, Color.Green,
+    Color.Green, Color.Green, Color.Green,
+    Color.Green, Color.Green, Color.Green,
 ]
 
 const backSide: Color[] = [
-  Color.Yellow, Color.Yellow, Color.Yellow,
-  Color.Yellow, Color.Yellow, Color.Yellow,
-  Color.Yellow, Color.Yellow, Color.Yellow,
-]
-
-const upSide: Color[] = [
-  Color.Red, Color.Red, Color.Red,
-  Color.Red, Color.Red, Color.Red,
-  Color.Red, Color.Red, Color.Red,
+    Color.Yellow, Color.Yellow, Color.Yellow,
+    Color.Yellow, Color.Yellow, Color.Yellow,
+    Color.Yellow, Color.Yellow, Color.Yellow,
 ]
 
 const downSide: Color[] = [
-  Color.Orange, Color.Orange, Color.Orange,
-  Color.Orange, Color.Orange, Color.Orange,
-  Color.Orange, Color.Orange, Color.Orange,  
+    Color.Red, Color.Red, Color.Red,
+    Color.Red, Color.Red, Color.Red,
+    Color.Red, Color.Red, Color.Red,
+]
+
+const upSide: Color[] = [
+    Color.Orange, Color.Orange, Color.Orange,
+    Color.Orange, Color.Orange, Color.Orange,
+    Color.Orange, Color.Orange, Color.Orange,
 ]
 
 const state = new RubiksCubeStateStructure(
-  frontSide,
-  lefttSide,
-  rightSide,
-  backSide,
-  upSide,
-  downSide
+    frontSide,
+    leftSide,
+    rightSide,
+    backSide,
+    upSide,
+    downSide
 )
 
 const turnService = new TurnService()
 const rubiksCube = new RubiksCube(state, turnService)
 const assembler = new RubiksCubeAssembler(rubiksCube)
 rubiksCube.Render()
-rubiksCube.turnFrontSideRight()
+// rubiksCube.turnFrontSideLeft()
+// rubiksCube.turnFrontSideRight()
+rubiksCube.turnLeftSideAway()
+// rubiksCube.turnLeftSideToward()
+// rubiksCube.turnRightSideToward()
 rubiksCube.Render()
 // assembler.Assemble();
-
